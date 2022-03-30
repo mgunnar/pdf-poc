07467ce04ebc186ea06de3f6b4bb6b0eab267c9a
mgunnar <devmatheus@gmail.com>
2022-03-30 10:02:32 -0300

    Criação de pasta pdf e instalação do react-pdf

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { PDFViewer, PDFRenderer } from "@react-pdf/renderer";
+import { Document, Page, Text, View, Image } from "@react-pdf/renderer";
+//import MyDocument from "./pdf/Teste";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <PDFViewer>
+    <MyDocument />
+  </PDFViewer>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
