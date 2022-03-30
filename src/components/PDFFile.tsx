97459aa054f10d36837b6b021dec324a1510107d
brunomdrrosa <brunomdr46@gmail.com>
2022-03-30 11:01:29 -0300

    First commit

---
+++ src/components/PDFFile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Page, Text, Image, Document, StyleSheet } from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({});
+
+const PDFFile = () => {
+  return (
+    <h1>teste</h1>
+  )
+};
+
+export default PDFFile;
